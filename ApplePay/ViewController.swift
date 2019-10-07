6d931242e2e27d8be03552231465ba642db0867a
M <manav@xebia.com>
2019-10-08 03:26:23 +0530

    First Commit

--- ApplePay/ViewController.swift
+++ ApplePay/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
-
+import PassKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addApplePayPaymentButtonToView()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    private func addApplePayPaymentButtonToView() {
+        let paymentButton = PKPaymentButton(paymentButtonType: .buy, paymentButtonStyle: .white)
+        paymentButton.translatesAutoresizingMaskIntoConstraints = false
+        paymentButton.addTarget(self, action: #selector(applePayButtonTapped(sender:)), for: .touchUpInside)
+        view.addSubview(paymentButton)
+        view.addConstraint(NSLayoutConstraint(item: paymentButton, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: paymentButton, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0))
+    }
+    
+    @objc private func applePayButtonTapped(sender: UIButton) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
